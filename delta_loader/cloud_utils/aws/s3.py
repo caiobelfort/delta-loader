b7174eac9b23ca5d28f16b34fd5c3ec906bebeca
Caio Belfort <caiobelfort90@gmail.com>
2022-09-11 23:47:55 -0300

    Modifiquei a função para retornar o timestamp de cada objeto.

--- delta_loader/cloud_utils/aws/s3.py
+++ delta_loader/cloud_utils/aws/s3.py
@@ -4,7 +4,17 @@ import typing
 
 def get_new_objects(client, bucket: str,
                     folder_path: str,
-                    last_modified: datetime.datetime) -> typing.Tuple[typing.List[str], datetime.datetime]:
+                    last_modified: datetime.datetime) -> typing.List[typing.Tuple[str, datetime]]:
+    """
+    Retorna lista de novos objetos criados no bucket e na pasta especificada a partir de uma data.
+
+    :param client: Client S3
+    :param bucket: Bucket S3
+    :param folder_path: Path da pasta no bucket
+    :param last_modified: Data de filtro
+    :return: Lista de tuplas com nome do objeto e data de modificação
+    """
+
     utc_tz = datetime.timezone.utc  # alias
 
     response = client.list_objects_v2(
@@ -12,14 +22,11 @@ def get_new_objects(client, bucket: str,
         StartAfter=folder_path
     )
 
-    objects = []
-    max_modified_date = datetime.datetime(1970, 1, 1, tzinfo=utc_tz)
+    objects: typing.List[typing.Tuple[str, datetime]] = []
 
     for obj in response['Contents']:
         modified_date = obj['LastModified']
         if modified_date > last_modified.replace(tzinfo=utc_tz):
-            objects.append(obj['Key'])
+            objects.append((obj['Key'], modified_date))
 
-        max_modified_date = max(max_modified_date, modified_date)
-
-    return objects, max_modified_date
+    return objects
