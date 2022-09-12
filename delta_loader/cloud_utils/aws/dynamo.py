87ea62b174119b99d79c7b8d860a78f5cbe271f6
Caio Belfort <caiobelfort90@gmail.com>
2022-09-12 01:28:19 -0300

    Adicionada funcionalidade de put_item no dynamo

--- delta_loader/cloud_utils/aws/dynamo.py
+++ delta_loader/cloud_utils/aws/dynamo.py
@@ -14,3 +14,20 @@ def get_item(client, table_name: str, key: dict) -> dict:
     )
 
     return response['Item']
+
+
+def put_item(client, table_name: str, item: dict) -> None:
+    """
+    Insere item na tabela DynamoDB.
+
+    :param client: Client DynamoDB
+    :param table_name: Nome da tabela
+    :param item: Item
+    :return: None
+    """
+    table = client.Table(table_name)
+    table.put_item(
+        TableName=table_name,
+        Item=item
+    )
+
